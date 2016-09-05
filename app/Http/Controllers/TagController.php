b98a952ede6b9b65cd99f140a94f9032265d0eb7
Artem <polikarpov_aj@groupbwt.com>
2016-09-05 11:45:59 +0300

    [controller][add]  Add action show for display articles by tag

--- app/Http/Controllers/TagController.php
+++ app/Http/Controllers/TagController.php
@@ -2,11 +2,13 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-
 use App\Http\Requests;
+use App\Models\Tag;
 
 class TagController extends Controller
 {
-    //
+    public function show($id)
+    {
+        return view('frontend.tag.show', ['articles' => Tag::find($id)->articles]);
+    }
 }
